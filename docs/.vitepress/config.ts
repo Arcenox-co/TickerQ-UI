f44b12791841cf117a0d1edf8305127740627ca0
Arcenox <albert@arcenox.com>
2025-04-19 18:03:22 +0200

    added version

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -14,6 +14,7 @@ export default defineConfig({
     nav: [
       { text: 'Home', link: '/' },
       { text: 'Donate', link: 'https://www.paypal.com/donate?hosted_button_id=RSRE2QUK2AT3Q' },
+      { text: 'v2.0.0', link: 'https://github.com/arcenox/TickerQ/releases/tag/v2.0.0' }
     ],
     sidebar: [
       {
