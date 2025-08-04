bec336633c883390638eb5babbb322de14bc5e19
Arcenox <albert@arcenox.com>
2025-08-04 17:35:56 +0200

    Added google analytics

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -3,7 +3,14 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   title: "TickerQ",
   head: [
-    ['link', { rel: 'icon', href: '/arcenox-logo.svg' }]
+    ['link', { rel: 'icon', href: '/arcenox-logo.svg' }],
+    ['script', { async: true, src: 'https://www.googletagmanager.com/gtag/js?id=G-D81DHQQ8WB' }],
+    ['script', {}, `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-D81DHQQ8WB');
+    `]
   ],
   themeConfig: {
     logo: '/arcenox-logo.png',
